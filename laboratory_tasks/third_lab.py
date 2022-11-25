dc4ddb65b95a7fca17dfdaaf2c280a545d4370c2
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2022-11-25 22:38:58 +1000

    fix NumberIntegration

--- laboratory_tasks/third_lab.py
+++ laboratory_tasks/third_lab.py
@@ -1,6 +1,11 @@
-from stuff.NumberIntegration.NumberIntegration import NumberIntegration
+from stuff.NumberIntegration.NumberIntegration import LeftBoxMethod, RightBoxMethod, MidBoxMethod, TrapezoidalMethod, \
+    SimpsonFormula
 from sympy import Symbol, ln
 
 
 def execute_third_lab():
     x = Symbol('x')
+    t = TrapezoidalMethod(10, 0.4, 0.9, x ** 2 + ln(x))
+
+    
+
--- stuff/NumberIntegration/NumberIntegration.py
+++ stuff/NumberIntegration/NumberIntegration.py
@@ -6,22 +6,69 @@ from sympy.core import add
 class NumberIntegration(BaseFunctionHandler):
 
     _exact_integral_of_the_function = 0
+    _calculated_integral_of_the_function = 0
     _value_of_method = 0
+    _absolute_error_value = 0
+    _relative_error_value = 0
+    _remainder_term_value = 0
 
     def __init__(self, n: int, begin_interval: float, end_interval: float, function: add):
         x = Symbol('x')
         super().__init__(n, begin_interval, end_interval, function)
-        self.__exact_integral_of_the_function = integrate(self._function, x)
+        self._exact_integral_of_the_function = integrate(self._function, x)
+        self._calculated_integral_of_the_function = self._exact_integral_of_the_function.subs(x, self._interval[
+            1]) - self._exact_integral_of_the_function.subs(x, self._interval[0])
+        self._execute_method()
+        self._calculate_absolute_error()
+        self._calculate_relative_error()
+        self._calculate_remainder_term()
+
+    def _execute_method(self):
+        pass
+
+    def _get_calculated_exact_integral(self):
+        return self._calculated_integral_of_the_function
+
+    def change_values(self, n: int, begin_interval: float, end_interval: float, function: add):
+        x = Symbol('x')
+        super().__init__(n, begin_interval, end_interval, function)
+        self._exact_integral_of_the_function = integrate(self._function, x)
+        self._calculated_integral_of_the_function = self._exact_integral_of_the_function.subs(x, self._interval[
+            1]) - self._exact_integral_of_the_function.subs(x, self._interval[0])
 
     def get_exact_integral(self):
-        return self.__exact_integral_of_the_function
+        return self._exact_integral_of_the_function
+
+    def get_value_of_method(self):
+        return self._value_of_method
+
+    def get_absolute_error(self):
+        return self._absolute_error_value
+
+    def get_relative_error(self):
+        return self._relative_error_value
+
+    def get_remainder_term(self):
+        return self._remainder_term_value
+
+    def _calculate_absolute_error(self):
+        x = Symbol('x')
+        self._absolute_error_value = abs(self._calculated_integral_of_the_function - self._value_of_method)
+
+    def _calculate_relative_error(self):
+        x = Symbol('x')
+        self._relative_error_value = (self._absolute_error_value / (abs(self._calculated_integral_of_the_function)))
+
+    def _calculate_remainder_term(self):
+        x = Symbol('x')
+
+        self._remainder_term_value = self._calculated_integral_of_the_function - self._value_of_method
 
 
 class LeftBoxMethod(NumberIntegration):
 
     def __init__(self, n: int, begin_interval: float, end_interval: float, function: add):
         super().__init__(n, begin_interval, end_interval, function)
-        self._execute_method()
 
     def _execute_method(self):
         sum_of_boxes = 0
@@ -34,7 +81,6 @@ class RightBoxMethod(NumberIntegration):
 
     def __init__(self, n: int, begin_interval: float, end_interval: float, function: add):
         super().__init__(n, begin_interval, end_interval, function)
-        self._execute_method()
 
     def _execute_method(self):
         sum_of_boxes = 0
@@ -47,7 +93,6 @@ class MidBoxMethod(NumberIntegration):
 
     def __init__(self, n: int, begin_interval: float, end_interval: float, function: add):
         super().__init__(n, begin_interval, end_interval, function)
-        self._execute_method()
 
     def _execute_method(self):
         x = Symbol('x')
@@ -90,4 +135,7 @@ class SimpsonFormula(NumberIntegration):
             parabols_sums = parabols_sums + parabola
 
         self._value_of_method = parabols_sums
+
+
+
 
